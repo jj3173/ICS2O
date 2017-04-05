7f6087a2817b00e37152b1135d10358a452391b2
jj3173 <jyang11@ocdsb.ca>
2017-04-05 13:13:10 -0400

    Update sketch.js

--- p5/House/sketch.js
+++ p5/House/sketch.js
@@ -11,70 +11,54 @@ function draw(){
 
 	background(195, 237, 250);
 
-	var posX = 300;
-	var posY =334;
-	var sizeEllipse =100;
+	var posX = 30;
+	var posY =280;
+	var houseSize =100;
 
 	//sun
-	noStroke();
-	fill(247, 210, 24);
-	ellipse(55,50,90,90);
-
-	// right cloud
-	fill(255, 255, 255);
-	ellipse(posX, posY-250, sizeEllipse+10, sizeEllipse);
-	ellipse(posX+70, posY-250, sizeEllipse-30, sizeEllipse-40);
-	ellipse(posX-70, posY-250, sizeEllipse-30, sizeEllipse-40);
-
+	fill(245, 234, 150);
+	ellipse(posX*2,posY/5, houseSize, houseSize);
 
 	//grass
-	fill(137, 235, 132);
-	rect(0,300,400,100);
+	fill(167, 245, 157);
+	rect(posX*0,posY+houseSize/1.5, houseSize*4, houseSize/1.5);
 
-	//path
-	noStroke();
-	fill(148, 143, 143);
-	rect(172, 323, 66, 115);
-
+	//House #1 
 	//wall
 	fill(250, 163, 180);
-	rect(60, 180, 280, 155);
-
-	//chimney
-	fill(102, 71, 45);
-	rect(99, 80, 33, 77);
-
-	//roof
-	fill(115, 84, 52);
-	triangle(200, 50, 350, 190, 50, 190);
+	rect(posX, posY, houseSize, houseSize);
 
 	//door
 	fill(51, 40, 23);
-	rect(172, 260, 65, 76);
+	rect(posX+houseSize/3, posY+houseSize/2, houseSize/3, houseSize/2);
 
-	//doorknob
-	fill(153, 147, 147);
-	ellipse(225,300,15,15);
+	//roof
+	fill(115, 84, 52);
+	triangle(posX-houseSize/10, posY+houseSize/10, posX+houseSize/2, posY-houseSize*0.5, posX+houseSize*1.1, posY+houseSize/10);
 
 	//window
-	fill(226, 235, 235);
-	rect(80, 205, 70, 60);
-	rect(255, 205, 70, 60);
-	stroke(97, 70, 49);
-	strokeWeight(5);
-	line(147,235,83, 235);
-	line(322,235,257, 235);
-	line(115, 262, 115, 207);
-	line(290, 262, 290, 207);
+	fill(94, 167, 209);
+	rect(posX+houseSize/10, posY+houseSize/6, houseSize/4, houseSize/4);
+	rect(posX+houseSize/1.5, posY+houseSize/6, houseSize/4, houseSize/4);
 
-	// bush
-	noStroke();
-	var length = 50;
-	fill(32, 61, 29);
-	ellipse(posX, posY, sizeEllipse-30, sizeEllipse-40);
-	ellipse(posX+40, posY, sizeEllipse/2, sizeEllipse/2);
-	ellipse(posX-40, posY, sizeEllipse/2, sizeEllipse/2);
-	ellipse(posX-200, posY, sizeEllipse-30, sizeEllipse-40);
-	ellipse(posX-240, posY, sizeEllipse/2, sizeEllipse/2);
-	ellipse(posX-160, posY, sizeEllipse/2, sizeEllipse/2);
+
+	//House #2 
+	//wall
+	fill(250, 163, 180);
+	rect(posX+houseSize*1.5, posY-houseSize, houseSize*2, houseSize*2);
+
+	//door
+	fill(51, 40, 23);
+	rect(posX+houseSize*2.2, posY-houseSize/80, houseSize/1.5, houseSize);
+
+	//roof
+	fill(115, 84, 52);
+	triangle(posX+houseSize*2.5, posY/4, posX+houseSize*1.3, posY/1.4, posX+houseSize*3.7, posY/1.4);
+
+	//window
+	fill(94, 167, 209);
+	rect(posX+houseSize*1.6, posY-houseSize/1.5, houseSize/1.75, houseSize/1.75);
+	rect(posX+houseSize*2.8, posY-houseSize/1.5, houseSize/1.75, houseSize/1.75);
+
+
 }
