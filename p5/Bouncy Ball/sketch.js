cd143cb19b1bae62ff82e484627bd765177faee9
jj3173 <jyang11@ocdsb.ca>
2017-04-20 11:44:58 -0400

    Update sketch.js

--- p5/Bouncy Ball/sketch.js
+++ p5/Bouncy Ball/sketch.js
@@ -1,7 +1,16 @@
+/* Bouncy Ball Assignment
+Jieru
+4/20/2017 */
+
+//I'm going to make a bouncy ball bouce from two sides of the wall
+
+
+//these are my global varaibles 
 var posX = 10;
 var posY = 10;
 var speedX = 3;
 var speedY = 3;
+//this variable is explained in the function setup
 var backColor;
 
 function setup() {
@@ -12,25 +21,30 @@ function setup() {
 
 function draw() {
 	background(backColor);
+	//the ball
 	ellipse(posX, posY, 20, 20);
+	//position of the ball
 	posX = posX + speedX;
 	posY = posY + speedY;
 
+		//the border of the frame
 		   if (posX > 390){
 		     speedX = -speedX;
+			   //changes color when it hits the border
 		     backColor = color(random(255), random(255), random(255));
 		   }  
 
+		//the border of the frame
 		   if (posY > 390){
 		     speedY= -speedY;
 		     backColor = color(random(255), random(255), random(255));
 		   }
-
+		//the border of the frame
 		   if (posX < 0){
 		     speedX = -speedX;
 		     backColor = color(random(255), random(255), random(255));
 		   }
-
+		//the border of the frame
 		  if(posY < 0) {
 		    speedY = -speedY;
 		    backColor = color(random(255), random(255), random(255));
