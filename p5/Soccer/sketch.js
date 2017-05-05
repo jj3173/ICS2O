fa9216cb7f73cdbb289975fa1c2877a2c86ce0c6
jj3173 <jyang11@ocdsb.ca>
2017-05-05 13:23:56 -0400

    Update sketch.js

--- p5/Soccer/sketch.js
+++ p5/Soccer/sketch.js
@@ -27,10 +27,11 @@ function draw() {
 	fill(0, 0, 0);
 	//size of the score
 	textSize(30);
+	
 	//Question 2 display scores
 	text(scoreA, 20, 30);
 	text(scoreB, 600, 30);
-
+	
 	//court lines
 	fill(255, 255, 255); //white
 	ellipse(320, 240, 200, 200);
@@ -39,48 +40,51 @@ function draw() {
 	//net
 	rect(0, 100, 10, 250); 
 	rect(630, 100, 10, 250); 
-
-
+	
 	//soccer ball
 	stroke(0, 0, 0);
+	
 	//ball is black in color
 	fill(0, 0, 0);
+	
 	//size and position of the ball 
 	ellipse (posX, posY, 20, 20);
-
+	
 	//the position of the ball 
 	posX = posX + speedX;
 	posY = posY + speedY;
-
+	
 	//check boundaries return to top left
 	if(posX >= width) {
-	  posX = 1;
-	  posY = 1;
-	speedX = 3;
-	speedY = 2;
-	scoreA = scoreA + 1 //score on the left - how many balls missed by Bob
+		posX = 1;
+		posY = 1;
+		speedX = 3;
+		speedY = 2;
+		scoreA = scoreA + 1 //score on the left - how many balls missed by Bob
 	}	
-
+	
 	if (posY>= height) {
-	  posX = 1;
-	  posY = 1;
-	speedX = 3;
-	speedY = 2;
+		posX = 1;
+		posY = 1;
+		speedX = 3;
+		speedY = 2;
 	}
+	
 	if (posX <= 0) {
-	  speedX = -speedX;
-	scoreB = scoreB+1 //score on the right - how many balls he kicked to the net
+		speedX = -speedX;
+		scoreB = scoreB+1 //score on the right - how many balls he kicked to the net
 	}
-
+	
 	if (posY <= 0) {
-	  speed = -speedY;
+		speed = -speedY;
 	}
-
+	
 	//mouse variables to access player
 	if (mouseIsPressed) {
-	  player();
+		player();
+		
 	//checking for collisions between player and ball
-	  if((abs(posX - mouseX) <30) && (abs(posY-mouseY)<30)) {
+	if((abs(posX - mouseX) <30) && (abs(posY-mouseY)<30)) {
 		speedX = -round(random(1,6));
 		speedY = round(random(-4, 4));
 		}
@@ -91,50 +95,34 @@ function draw() {
 
 function player () {
 	//creating a function for drawing of the soccer player
-
         //head
-	fill(255, 247, 0);
-	
+	fill(255, 247, 0);	
 	ellipse(mouseX, mouseY, faceSize, faceSize);
 	
-
 	//eyes
 	fill(0, 0, 0);
-	
 	ellipse(mouseX-faceSize/6, mouseY-faceSize/8, faceSize/4, faceSize/4);
-	
 	ellipse(mouseX+faceSize/6, mouseY-faceSize/8, faceSize/4, faceSize/4);
 
-	
 	//eye white
 	fill(255,255,255);
-	
 	ellipse(mouseX-faceSize/6, mouseY-faceSize/8, faceSize/6, faceSize/6);
-	
 	ellipse(mouseX+faceSize/6, mouseY-faceSize/8, faceSize/6, faceSize/6);
-
 
 	//mouth	
 	fill (230, 0, 255);	
-	
 	ellipse(mouseX+faceSize/30, mouseY+faceSize/4, faceSize/2, faceSize/4);
 
-	
 	//body
 	fill(34, 0, 255);
 	rect(mouseX-faceSize/2, mouseY+faceSize/2, 30, 40);
 
 	//legs
 	fill(54, 207, 171);
-	
 	rect(mouseX-faceSize/3, mouseY+faceSize*1.8, faceSize/4, faceSize/2);
-	
 	rect(mouseX+faceSize/8, mouseY+faceSize*1.8, faceSize/4, faceSize/2);
-
 
 	//arms
 	rect(mouseX-faceSize, mouseY+faceSize, faceSize/2, faceSize/4);
-	
 	rect(mouseX+faceSize/2, mouseY+faceSize, faceSize/2, faceSize/4);
-
 }
