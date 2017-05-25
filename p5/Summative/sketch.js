c8e7bc2a3dedb98e24bf574dc27c64ba392e532f
jj3173 <jyang11@ocdsb.ca>
2017-05-25 13:29:27 -0400

    Update sketch.js

--- p5/Summative/sketch.js
+++ p5/Summative/sketch.js
@@ -8,11 +8,23 @@ function setup() {
 	gamePlayer = loadImage("Mario.png");
 }
 
-
-
-
 function draw() {
 	background(bg);
 	image(gamePlayer, 0, height/4, gamePlayer.width/12, gamePlayer.height/12);
+}
+
+function keyPressed() {
+	if (keyCode == RIGHT_ARROW) {
+		posX += 6;
+	}
+	else if (keyCode == LEFT_ARROW) {
+		posX -= 6;
+	}
+	else if (keyCode == UP_ARROW) {
+		posY -= 6;
+	}
+	else if (keyCode == DOWN_ARROW) {
+		posY += 6;
+	}
 }
 
