9d87f6167d9d7b24c753b141f64cac4e446a380a
jj3173 <jyang11@ocdsb.ca>
2017-06-09 14:03:06 -0400

    Create sketch.js

--- p5/Summative/sketch.js
+++ p5/Summative/sketch.js
@@ -168,7 +168,7 @@ function keyPressed() {
 	}
 	//if the up arrow is pressed, Mario will move up
 	else if (keyCode == UP_ARROW) {
-		posY -= 50;
+		posY -= 100;
 	}
 	//if the down arrow is pressed, Mario will move down
 	else if (keyCode == DOWN_ARROW) {
