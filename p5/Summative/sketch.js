e5d9aa823a6aeee12f1ec7e9765995df4e18fadb
jj3173 <jyang11@ocdsb.ca>
2017-06-09 13:43:27 -0400

    Create sketch.js

--- p5/Summative/sketch.js
+++ p5/Summative/sketch.js
@@ -137,7 +137,7 @@ function draw() {
 		speedX = 0;	
 	}
 	//checking for collision between Mario and the donut
-	if ((abs(posX-donutX)<10) && (abs(posY-donutY)<10)) {
+	if ((abs(posX-donutX)<100) && (abs(posY-donutY)<100)) {
 		score = score + 2;
 	}
 }
